b02bafaeb2de19d9435b8d3ee7a92bbdfabd7e10
C-Karki <karkic@gmail.com>
2020-12-16 18:36:16 +0545

    updating stuff

--- chart.js
+++ chart.js
@@ -1,7 +1,29 @@
+const api_nepal = 'https://nepalcorona.info/api/v1/data/nepal';
+const api_timeline_url = 'https://data.nepalcorona.info/api/v1/covid/timeline';
+const api_nepal_summary = 'https://data.nepalcorona.info/api/v1/covid/summary' ;
+      async function getCovidData() {
+          const response = await fetch(api_nepal);
+          const data = await response.text();
+	  const total_cases = data.tested_positive;
+	  const date = [];
+	  const totalDeaths = [];
+	  const totalRecoveries = [];
+	  const newCases = [];
+//	  data.forEach(newCases => tCases.push(newCases.totalCases));
+	  const x = JSON.parse(data);
+	  console.log(x);
+	  document.getElementById('totalcases').textContent = x.tested_positive;
+	  document.getElementById('recoveries').textContent = x.recovered;
+	  document.getElementById('deaths').textContent = x.deaths;
+	  document.getElementById('isolation').textContent = x.in_isolation;
+	  document.getElementById('quarantine').textContent = x.quarantined;
+      }
+getCovidData();
+
 window.addEventListener('load', setup);
 
       async function setup() {
-        const ctx = document.getElementById('myChart').getContext('2d');
+	  const ctx = document.getElementById('myChart').getContext('2d');
         const globalTemps = await getData();
         const myChart = new Chart(ctx, {
           type: 'line',
